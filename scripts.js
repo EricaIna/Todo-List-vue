170425303b35d59343bb9bee61e4c95f782748a6
EricaIna <erica.rirura.4you@gmail.com>
2023-08-12 19:17:07 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+var todos = [
+  {
+    text: "Learn HTML, CSS and Javascript",
+    done: true,
+  },
+  {
+    text: "Learn the basics of Vue JS",
+    done: false,
+  },
+];
+
+const todosApp = {
+  data() {
+    return {
+      todos: [],
+      newTodo: {
+        done: false,
+      },
+    };
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodo.text) {
+        this.todos.push(this.newTodo);
+        this.newTodo = {
+          done: false
+        };
+      } else {
+        alert("To do text is required");
+      }
+    },
+  },
+};
+
+Vue.createApp(todosApp).mount("#app");
